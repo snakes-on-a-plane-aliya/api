ea551f4bffaa24f6d1699cc4e8c7a3991f7675b8
arsummers <aliyasummers1@gmail.com>
2019-08-20 14:53:45 -0700

    flight views written

--- api/settings.py
+++ api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     # models
     'flightseats.apps.FlightseatsConfig',
+    'api_app.apps.ApiAppConfig',
 ]
 
 MIDDLEWARE = [
--- api_app/views.py
+++ api_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics
+from flightseats.models import Flight, Seat, Passenger
+from .serializers import FlightSerializer, SeatSerializer, PassengerSerializer
 
-# Create your views here.
+class FlightList(generics.ListCreateAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+class FlightDetail(generics.RetrieveUpdateAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+class SeatList(generics.ListCreateAPIView):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+class SeatDetail(generics.RetrieveUpdateAPIView):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+class PassengerList(generics.ListCreateAPIView):
+    queryset = Passenger.objects.all()
+    serializer_class = PassengerSerializer
